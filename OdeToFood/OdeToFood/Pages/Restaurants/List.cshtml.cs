06d03efb4168a7e3fd67242d9958801ac6b95ce2
Scott Allen <scott@odetocode.com>
2018-10-22 16:03:56 -0400

    added page logic to show a message

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public void OnGet()
         {
+            Message = config["Message"];
         }
     }
 }
