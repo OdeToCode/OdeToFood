877546899fe36804a20aa07e8b2ac74df6ca67b9
Scott Allen <scott@odetocode.com>
2018-10-23 09:38:40 -0400

    added restarurant type

---
+++ OdeToFood/OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
---
+++ OdeToFood/OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
