2cdd8270e0f082037529fbc9b969dd564b408158
Scott Allen <scott@odetocode.com>
2018-10-23 14:23:43 -0400

    registering iRestaurantData

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,18 +5,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -24,6 +25,8 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
