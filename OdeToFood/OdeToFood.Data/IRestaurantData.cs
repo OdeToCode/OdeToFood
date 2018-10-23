05e037b18c4f834e5bcbb1dbf7b362502a7d5031
Scott Allen <scott@odetocode.com>
2018-10-23 14:16:31 -0400

    added data project

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using OdeToFood.Core;
+using System.Collections.Generic;
+
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location="London", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine=CuisineType.Mexican}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
