d54d10abb36ffd5211833221d1e9ed3774941ba2
Scott Allen <scott@odetocode.com>
2018-10-26 15:35:39 -0400

    implement search in data service

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -7,7 +7,7 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -24,9 +24,10 @@ namespace OdeToFood.Data
             };
         }
 
-        public IEnumerable<Restaurant> GetAll()
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
         }
