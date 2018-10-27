27b783c315393ba1febc15464b5ad86db0e4dede
Scott Allen <scott@odetocode.com>
2018-10-27 17:05:59 -0400

    adding link to details

--- OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -12,9 +12,10 @@ namespace OdeToFood.Pages.Restaurants
     {
         public Restaurant Restaurant { get; set; }
 
-        public void OnGet()
+        public void OnGet(int restaurantId)
         {
             Restaurant = new Restaurant();
+            Restaurant.Id = restaurantId;
         }
     }
 }
