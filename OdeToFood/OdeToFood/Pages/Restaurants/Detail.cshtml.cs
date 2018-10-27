0e32b032c4b3fc0303322f9a8bd836e78e02165d
Scott Allen <scott@odetocode.com>
2018-10-27 18:43:08 -0400

    implement NotFound

--- OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -20,9 +20,14 @@ namespace OdeToFood.Pages.Restaurants
             this.restaurantData = restaurantData;
         }
 
-        public void OnGet(int restaurantId)
+        public IActionResult OnGet(int restaurantId)
         {
             Restaurant = restaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
         }
     }
 }
