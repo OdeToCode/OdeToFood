6956caef48d646048b62cd08e1f3df92b3dfb5a2
Scott Allen <scott@odetocode.com>
2018-10-27 09:33:24 -0400

    add detail page

---
+++ OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+
+        public void OnGet()
+        {
+            Restaurant = new Restaurant();
+        }
+    }
+}
